3f725cef7916ee8001855a30fcde2fcdbdd360f9
piyoppa <piyoppa@gmail.com>
2021-09-01 13:30:34 +0900

    first commit

---
+++ src/main/java/com/example/Ec202107bApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ec202107bApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ec202107bApplication.class, args);
+	}
+
+}
