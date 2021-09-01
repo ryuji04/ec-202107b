13d5c4b96d4767789cd27d71c404fff5eed04740
市野　那由他 <67714705+nayuta-ichino@users.noreply.github.com>
2021-09-01 15:04:11 +0900

    ShowItemDetailServiceの作成途中.

---
+++ src/main/java/com/example/service/ShowItemDetailService.java
@@ -0,0 +1,16 @@
+package com.example.service;
+
+import com.example.repository.ToppingRepository;
+
+/**
+ * itemsテーブルとtoppingsテーブルを操作するService.
+ * 
+ * @author nayuta
+ */
+public class ShowItemDetailService {
+	// ToppingRepositoryをインスタンス化
+	private ToppingRepository toppingRepository;
+	// ItemRepositoryをインスタンス化
+	// private ItemRepository itemRepository;
+
+}
