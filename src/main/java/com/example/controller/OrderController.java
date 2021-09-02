dae82fc62e29dc6e7d91a1e0c85b53cf6e108863
市野　那由他 <67714705+nayuta-ichino@users.noreply.github.com>
2021-09-02 15:56:08 +0900

    商品注文 途中

--- src/main/java/com/example/controller/OrderController.java
+++ src/main/java/com/example/controller/OrderController.java
@@ -1,5 +1,44 @@
 package com.example.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.domain.Order;
+import com.example.form.OrderForm;
+import com.example.service.OrderService;
+
+/**
+ * ordersテーブルを操作するcontroller.
+ * 
+ * @author nayuta
+ */
+@Controller
+@RequestMapping("/order-item")
 public class OrderController {
+	@Autowired
+	private OrderService service;
+
+	@RequestMapping("/index")
+	public String index() {
+		return "order_confirm.html";
+	}
+
+	/**
+	 * 注文最終確認画面.
+	 * 
+	 * @param form
+	 * @return
+	 */
+	@RequestMapping("/order")
+	public String order(OrderForm form, Model model) {
+		// orderに情報変更後のorderを格納
+		Order order = service.upDateOrder(form);
+
+		model.addAttribute(order);
+
+		return "redirect:order_finished.html";
+	}
 
 }
--- src/main/java/com/example/service/OrderService.java
+++ src/main/java/com/example/service/OrderService.java
@@ -1,5 +1,46 @@
 package com.example.service;
 
-public class OrderService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.example.domain.Order;
+import com.example.form.OrderForm;
+import com.example.repository.OrderRepository;
+
+/**
+ * ordersテーブルを操作するService.
+ * 
+ * @author nayuta
+ */
+@Service
+@Transactional
+public class OrderService {
+	@Autowired
+	private OrderRepository repository;
+
+	/**
+	 * order情報を変更するメソッド.
+	 * 
+	 * @param form
+	 * @return 宛先名や宛先住所、状態の情報変更後のorder
+	 */
+	public Order upDateOrder(OrderForm form) {
+		// 注文確認画面に表示されていたOrderを取得
+		Order order = repository.findById(form.getId());
+
+		// 支払い方法によってstatusを変更
+		if (form.getPaymentMethod() == 1) {
+			repository.upDate(order);
+			order.setStatus(1);
+//			repository.upDateStatus1();
+		} else {
+			repository.upDate(order);
+			order.setStatus(2);
+//			repository.upDateStatus2();
+		}
+
+		// 変更後のorderをreturn
+		return order;
+	}
 }
