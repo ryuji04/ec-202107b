d54fe62e36aeabe990a1fbfe07f66087502ef61d
saishuhayato <hayato.saishu@rakus.partners.co.jp>
2021-09-02 11:31:00 +0900

    リスト内の商品数によって表示が変わってしまう問題を修正

--- src/main/java/com/example/service/ShowItemService.java
+++ src/main/java/com/example/service/ShowItemService.java
@@ -17,47 +17,32 @@ public class ShowItemService {
 	@Autowired
 	private ItemRepository itemRepository;
 
+	/**
+	 * 商品一覧を表示する.
+	 * 
+	 * @return アイテム情報のリスト
+	 */
 	public List<List<Item>> showList() {
 		List<List<Item>> totalItemList = new ArrayList<>();
 		List<Item> divideItemList = new ArrayList<>();
 		List<Item> itemList = itemRepository.findAll();
-		int roopCount = 0;
-
-		for (Item item : itemList) {
-			divideItemList.add(item);
-			roopCount++;
-			if (roopCount % 3 == 0) {
-				totalItemList.add(divideItemList);
-				divideItemList = new ArrayList<>();
-			}
-		}
+		
+		listItem(totalItemList, divideItemList, itemList);
 		return totalItemList;
 	}
 
+	/**
+	 * 名前検索に当てはまる商品一覧を表示する.
+	 * 
+	 * @param name 商品名
+	 * @return アイテム情報のリスト
+	 */
 	public List<List<Item>> searchByLikeName(String name) {
 		List<List<Item>> totalItemList = new ArrayList<>();
 		List<Item> divideItemList = new ArrayList<>();
 		List<Item> itemList = itemRepository.findByLikeName(name);
-		int roopCount = 0;
 
-		for (Item item : itemList) {
-			divideItemList.add(item);
-			roopCount++;
-
-			if (itemList.size() == 1) {
-				totalItemList.add(divideItemList);
-			} else if (itemList.size() == 2) {
-				if (roopCount == 2) {
-					totalItemList.add(divideItemList);
-				}
-			}
-			divideItemList.add(item);
-			roopCount++;
-			if (roopCount % 3 == 0) {
-				totalItemList.add(divideItemList);
-				divideItemList = new ArrayList<>();
-			}
-		}
+		listItem(totalItemList, divideItemList, itemList);
 		return totalItemList;
 	}
 
@@ -70,39 +55,41 @@ public class ShowItemService {
 		List<List<Item>> totalItemList = new ArrayList<>();
 		List<Item> divideItemList = new ArrayList<>();
 		List<Item> itemList = itemRepository.arrangeInDesc();
-		int roopCount = 0;
-
-		for (Item item : itemList) {
-			divideItemList.add(item);
-			roopCount++;
-			if (roopCount % 3 == 0) {
-				totalItemList.add(divideItemList);
-				divideItemList = new ArrayList<>();
-			}
-		}
+		
+		listItem(totalItemList, divideItemList, itemList);
 		return totalItemList;
 	}
-	
+
 	/**
 	 * アイテム商品を昇順に並び替える.
 	 * 
-	 * @return　並び替え後のアイテム商品リスト
+	 * @return 並び替え後のアイテム商品リスト
 	 */
 	public List<List<Item>> arrangeInAsc() {
 		List<List<Item>> totalItemList = new ArrayList<>();
 		List<Item> divideItemList = new ArrayList<>();
 		List<Item> itemList = itemRepository.arrangeInAsc();
-		int roopCount = 0;
-
-		for (Item item : itemList) {
-			divideItemList.add(item);
-			roopCount++;
-
-			if (roopCount % 3 == 0) {
+		
+		listItem(totalItemList, divideItemList, itemList);
+		return totalItemList;
+	}
+	
+	
+	//商品を3つずつリストに格納するメソッド
+	public void listItem(List<List<Item>> totalItemList, List<Item> divideItemList, List<Item> itemList) {
+		for (int i = 0; i < itemList.size(); i++) {
+			divideItemList.add(itemList.get(i));
+			
+			if ((i + 1) % 3 == 0) {
 				totalItemList.add(divideItemList);
 				divideItemList = new ArrayList<>();
 			}
+			
+			if (itemList.size() == i + 1) {
+				totalItemList.add(divideItemList);
+				break;
+			}
 		}
-		return totalItemList;
 	}
+	
 }
