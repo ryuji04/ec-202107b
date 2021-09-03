4369db312effacff97ba2786520c79ff69707c57
saishuhayato <hayato.saishu@rakus.partners.co.jp>
2021-09-03 16:04:58 +0900

    ログイン情報を取得するメソッドを変更

--- src/main/java/com/example/controller/ShowItemCartController.java
+++ src/main/java/com/example/controller/ShowItemCartController.java
@@ -1,12 +1,12 @@
 package com.example.controller;
 
-import javax.servlet.http.HttpSession;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.domain.LoginUser;
 import com.example.domain.Order;
 import com.example.domain.User;
 import com.example.service.ShowitemCartService;
@@ -17,12 +17,10 @@ public class ShowItemCartController {
 	@Autowired
 	private ShowitemCartService service;
 
-	@Autowired
-	private HttpSession session;
 
 	@RequestMapping("/cart")
-	public String showItemCart(Model model) {
-		User user = (User) session.getAttribute("user");
+	public String showItemCart(Model model, @AuthenticationPrincipal LoginUser loginUser) {
+		User user = loginUser.getUser();
 
 		// status=0の商品を取得
 		Order order = service.showItemCart(user.getId(), 0);
