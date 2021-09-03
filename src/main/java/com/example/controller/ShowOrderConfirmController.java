e681fba34fc1fefdeb8a257e993419a7487088e9
saishuhayato <hayato.saishu@rakus.partners.co.jp>
2021-09-03 17:29:05 +0900

    注文確認画面の表示

--- src/main/java/com/example/controller/ShowOrderConfirmController.java
+++ src/main/java/com/example/controller/ShowOrderConfirmController.java
@@ -2,25 +2,32 @@ package com.example.controller;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.domain.LoginUser;
 import com.example.domain.Order;
-import com.example.service.ShowOrderConfirmService;
+import com.example.domain.User;
+import com.example.service.ShowitemCartService;
 
 @Controller
 @RequestMapping("/order-confirm")
 public class ShowOrderConfirmController {
 
 	@Autowired
-	private ShowOrderConfirmService showOrderConfirmService;
+	private ShowitemCartService showitemCartService;
 	
 	@RequestMapping("")
-	public String searchById(Integer id, Model model) {
-		Order order = showOrderConfirmService.searchById(id);
+	public String searchById(Model model, @AuthenticationPrincipal LoginUser loginUser) {
+		User user = loginUser.getUser();
+
+		// status=0の商品を取得
+		Order order = showitemCartService.showItemCart(user.getId(), 0);
+
 		model.addAttribute("order", order);
-		
+
 		return "order_confirm";
 	}
 }
