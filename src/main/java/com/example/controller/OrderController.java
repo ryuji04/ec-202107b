5c392c1139f74ebff46acf3662340491ec557384
piyoppa <piyoppa@gmail.com>
2021-09-06 00:27:29 +0900

    [add] 合計金額機能追加

--- src/main/java/com/example/controller/OrderController.java
+++ src/main/java/com/example/controller/OrderController.java
@@ -1,6 +1,7 @@
 package com.example.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -8,6 +9,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.domain.LoginUser;
 import com.example.domain.Order;
 import com.example.form.OrderForm;
 import com.example.service.OrderService;
@@ -22,7 +24,7 @@ import com.example.service.OrderService;
 public class OrderController {
 	
 	@Autowired
-	private OrderService service;
+	private OrderService orderService;
 	
 	@ModelAttribute
 	public OrderForm setUpForm() {
@@ -30,7 +32,7 @@ public class OrderController {
 	}
 
 	@RequestMapping("/index")
-	public String index() {
+	public String index(Model model) {
 		return "order_confirm.html";
 	}
 
@@ -41,12 +43,12 @@ public class OrderController {
 	 * @return
 	 */
 	@RequestMapping("/order")
-	public String order(@Validated OrderForm form ,BindingResult result, Model model) {
+	public String order(@Validated OrderForm form ,BindingResult result, Model model, @AuthenticationPrincipal LoginUser loginUser) {
 		if(result.hasErrors()) {
-			return index();
+			return index(model);
 		}
 		// orderに情報変更後のorderを格納
-		Order order = service.upDateOrder(form);
+		Order order = orderService.upDateOrder(form);
 		model.addAttribute(order);
 		//後で注文完了画面にいけるようなメソッドにredirectする
 		return "redirect:/order-item/finished-order";
--- src/main/java/com/example/controller/ShowOrderConfirmController.java
+++ src/main/java/com/example/controller/ShowOrderConfirmController.java
@@ -9,9 +9,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.domain.LoginUser;
 import com.example.domain.Order;
+import com.example.domain.OrderItem;
 import com.example.domain.User;
 import com.example.service.ShowitemCartService;
 
+/**
+ * 注文確認画面を操作するcontroller.
+ * 
+ * @author saisyuhayato
+ */
 @Controller
 @RequestMapping("/order-confirm")
 public class ShowOrderConfirmController {
@@ -25,6 +31,13 @@ public class ShowOrderConfirmController {
 
 		// status=0の商品を取得
 		Order order = showitemCartService.showItemCart(user.getId(), 0);
+
+		// orderのtotalPriceを取得
+		int totalPrice = 0;
+		for( OrderItem item : order.getOrderItemList()) {
+			totalPrice += item.getSubTotal();
+		}
+		order.setTotalPrice(totalPrice);
 
 		model.addAttribute("order", order);
 
