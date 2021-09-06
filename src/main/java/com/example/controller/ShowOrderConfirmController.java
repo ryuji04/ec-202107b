bee8bcd7eb75e38eff643f2cd94e34430295f7e9
piyoppa <piyoppa@gmail.com>
2021-09-06 12:09:42 +0900

    [fix] 警告削除

--- src/main/java/com/example/controller/ShowOrderConfirmController.java
+++ src/main/java/com/example/controller/ShowOrderConfirmController.java
@@ -1,14 +1,9 @@
 package com.example.controller;
 
-
-import java.util.Date;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
